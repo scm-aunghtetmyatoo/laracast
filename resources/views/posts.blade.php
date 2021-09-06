1a66560ba2f22bc748e705d80d2b447f44ee58ad
P-000101\Aung Htet Myat Oo <scm.aunghtetmyatoo@gmail.com>
2021-09-06 22:05:40 +0630

    Make a route and Link to it (Section 02)

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="app.css">
+    <title>Blog</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!</p>
+    </article>
+</body>
+</html>
