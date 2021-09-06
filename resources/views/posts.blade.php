84611f942da222a309548e43b967bda706b19832
P-000101\Aung Htet Myat Oo <scm.aunghtetmyatoo@gmail.com>
2021-09-06 22:49:55 +0630

    Store blog posts as HTML files (section 02)

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,15 +6,15 @@
 </head>
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
+        <h1><a href="posts/my-first-post">My First Post</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!</p>
     </article>
     <article>
-        <h1><a href="/post">My Second Post</a></h1>
+        <h1><a href="posts/my-second-post">My Second Post</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!</p>
     </article>
     <article>
-        <h1><a href="/post">My Third Post</a></h1>
+        <h1><a href="posts/my-third-post">My Third Post</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus ducimus voluptatum aspernatur sed optio ipsum magnam sit temporibus. Aliquid, dolorem. Earum alias illum debitis. Ullam tempore quas eos possimus!</p>
     </article>
 </body>
