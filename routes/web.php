ddd732d8a547df5c099cbd2b9ecc6cb8bc3b9353
P-000101\Aung Htet Myat Oo <scm.aunghtetmyatoo@gmail.com>
2021-09-10 08:11:25 +0630

    Use Caching for Expensive Operations (section 02)

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,14 @@ Route::get('/', function () {
     return view('posts');
 });
 
-Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    if (!file_exists($path)) {
-        return redirect('/');
+Route::get('posts/{post}',function($slug){
+    if(!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html"))
+    {
+        abort(404);
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{slug}", 1200, fn() => file_get_contents($path));
 
-    return view('post', [
-        'post' => $post,
-    ]);
-})->where('post', '[A-z_\-]+'); //regular expression; post = line no 30 'post';
+    return view('post',['post' => $post,]);
+
+})->where('post', '[A-z_\-]+');//regular expression; post = line no 28 'post'   
